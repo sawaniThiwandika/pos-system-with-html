1d1e1d53759df5d03bd1b86845c369d0bc4293f2
User <thiwandika.whs02@gmail.com>
2024-05-09 00:41:12 +0530

    update item details

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,8 +1,18 @@
 import {ItemModel} from "../model/ItemModel.js";
-import {itemList} from "../db/db.js";
+import { itemList} from "../db/db.js";
 let item;
+loadId();
+let selectedIndex;
 function loadId() {
-
+    let itemCode;
+    if (itemList.length === 0) {
+        itemCode = "I-" + 1;
+    } else {
+        let lastItemCodeNumericPart = parseInt(itemList[itemList.length - 1].itemCode.substring(2));
+        let newNumericPart = lastItemCodeNumericPart + 1;
+        itemCode = "I-" + newNumericPart;
+    }
+    $('#itemCodeField').val(itemCode);
 }
 function loadItemTable() {
     $('#itemTableBody').empty();
@@ -33,6 +43,7 @@ $('#submitItemBtn').on('click', (event) => {
 
     item =new ItemModel(itemCode,unitPrice,category,itemName,itemQty);
     itemList.push(item);
+    $('#resetItemBtn').click();
     loadItemTable();
     console.log(item);
 });
@@ -41,12 +52,53 @@ $('#submitItemBtn').on('click', (event) => {
 
 $('#resetItemBtn').on('click',(event)=>{
     event.preventDefault();
-    loadId();
     $('#itemCodeField').val("");
     $('#itemNameField').val("");
     $('#itemQtyField').val("");
     $('#itemCategoryField').val("");
     $('#itemUnitPriceField').val("");
     console.log("reset customer details");
+    loadId();
 
+});
+$('#updateItemBtn').on('click', (event) => {
+    console.log("Before preventDefault()");
+    event.preventDefault();
+    console.log("After preventDefault()");
+    let itemCode = $('#itemCodeField').val();
+    let unitPrice = $('#itemUnitPriceField').val();
+    let category = $('#itemCategoryField').val();
+    let itemName = $('#itemNameField').val();
+    let itemQty = $('#itemQtyField').val();
+
+    if (selectedIndex !== undefined && selectedIndex >= 0 && selectedIndex < itemList.length) {
+        let selectItem = itemList[selectedIndex];
+        selectItem.itemCode =itemCode;
+        selectItem.itemName = itemName;
+        selectItem.unitPrice = unitPrice;
+        selectItem.category =category;
+        selectItem.itemQty = itemQty;
+        console.log(selectItem.itemCode);
+
+        $('#resetItemBtn').click();
+        loadItemTable();
+    } else {
+        console.log("No row selected or invalid index");
+    }
+});
+$('#itemTableBody').on('click','tr',function () {
+    let index = $(this).index();
+    selectedIndex= index;
+    let itemCode = $(this).find(".colItemCode").text();
+    let itemName = $(this).find(".colItemName").text();
+    let itemQty = $(this).find(".colItemQty").text();
+    let unitPrice = $(this).find(".colItemUnitPrice").text();
+    let category = $(this).find(".colItemCategory").text();
+    console.log("clicked row " + index);
+    $('#itemCodeField').val(itemCode);
+    $('#itemNameField').val(itemName);
+    $('#itemUnitPriceField').val(unitPrice);
+    $('#itemQtyField').val(itemQty);
+    $('#itemCategoryField').val(category);
+    $('#resetCusBtn').click();
 });
