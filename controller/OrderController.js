ae66f0a92c2f37b11356b92bccb84af049146f86
User <thiwandika.whs02@gmail.com>
2024-05-09 23:33:20 +0530

    calculate total price of a item

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -6,12 +6,10 @@ $(document).ready(function() {
         populateDatalist();
     });
 
-
     $("#itemNameFieldInOrder").on("change", function() {
         var selectedValue = $(this).val();
         updateOtherFields(selectedValue);
     });
-
 
     $("#itemNameFieldInOrder").on("input", function() {
         var selectedValue = $(this).val();
@@ -19,40 +17,45 @@ $(document).ready(function() {
             clearOtherFields();
         }
     });
+
+    $("#itemQtyFieldOrder").on("input", function() {
+        updateTotalPrice();
+    });
 });
 
 function populateDatalist() {
-
     var datalistForItems = $("#itemListForOrder");
-
-
     datalistForItems.empty();
-
-
     $.each(itemList, function(index, item) {
         datalistForItems.append($("<option>", { value: item.itemName }));
     });
-
 }
 
 function updateOtherFields(selectedValue) {
-
     var selectedItem = itemList.find(function(item) {
         return item.itemName === selectedValue;
     });
 
     if (selectedItem) {
-
-        $("#validationDefault08").val(selectedItem.unitPrice);
-
-
-        $("#validationDefault05").val(selectedItem.itemCode);
+        $("#itemUnitPriceFieldOrder").val(selectedItem.unitPrice);
+        $("#itemCodeFieldOrder").val(selectedItem.itemCode);
     }
 }
 
 function clearOtherFields() {
-
-    $("#validationDefault08").val("");
-    $("#validationDefault05").val("");
+    $("#itemUnitPriceFieldOrder").val("");
+    $("#itemCodeFieldOrder").val("");
+    $("#itemQtyFieldOrder").val("");
+    $("#totalPriceFieldInOrder").val("");
 }
 
+function updateTotalPrice() {
+    var unitPrice = parseFloat($("#itemUnitPriceFieldOrder").val());
+    var quantity = parseInt($("#itemQtyFieldOrder").val());
+    var totalPrice = unitPrice * quantity;
+    if (!isNaN(totalPrice)) {
+        $("#totalPriceFieldInOrder").val(totalPrice.toFixed(2));
+    } else {
+        $("#totalPriceFieldInOrder").val("");
+    }
+}
